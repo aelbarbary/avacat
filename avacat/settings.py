9cd3a9367f11c6caec9465dfd629f4a883f9282b
aelbarbary <abdelrahman.elbarbary@gmail.com>
2017-06-06 00:08:32 -0700

    remove sslserver

--- avacat/settings.py
+++ avacat/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'avacatapp',
-    'registration',
-    "sslserver",
+    'registration'
 ]
 
 MIDDLEWARE = [
