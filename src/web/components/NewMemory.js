73ccb8d9eab7b61a51d3fe34a09c0c5f68d89f26
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-06-05 16:04:53 -0700

    save memory

--- src/web/components/NewMemory.js
+++ src/web/components/NewMemory.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Firebase, FirebaseRef } from '../../lib/firebase';
 
 class NewMemory extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      
+
     };
+
+    this.guid.bind(this);
+    this.s4.bind(this);
+  }
+
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  guid() {
+   return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
   }
 
   handleChange(e){
@@ -14,6 +28,19 @@ class NewMemory extends Component {
   }
   handleClick(e){
     console.log(this.state);
+
+
+    const UID = Firebase.auth().currentUser.uid;
+    if (!UID) return false;
+
+      const ref = FirebaseRef.child(`memories`);
+      const guid = this.guid()
+      ref.set({
+        [guid]: {
+          memory: this.state.memory
+        }
+      });
+
   }
   render() {
     {this.state.data}
